f5c1b7824f42e83a4315e94ac500945944197bde
Kaewan Gardi <k1gardi@gmail.com>
2022-04-14 19:28:20 -0700

    enable Typescript and Prettier

---
+++ client/src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css';
+import { useEffect } from 'react';
+
+export const App = () => {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8084/api/users/current_user', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    fetch('http://127.0.0.1:8084/api/jobs', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
