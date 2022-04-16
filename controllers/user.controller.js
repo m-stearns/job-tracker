8a9bac4bc97c38c1a6ab16c569544d855d3fc5a1
Adam <okashaa@oregonstate.edu>
2022-04-16 09:04:01 -0400

    Add error handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,12 +2,16 @@ const { User } = require("../models");
 
 class UserController {
   static async create(req, res) {
-    const { name, email, password } = req.body;
+    try {
+      const { name, email, password } = req.body;
 
-    const user = await User.create({ name, email, password });
+      const user = await User.create({ name, email, password });
 
-    res.json({ user, auth_token: user.genAuthToken() });
+      res.json({ user, auth_token: user.genAuthToken() });
+    } catch (error) {
+      res.status(400).send({ error });
+    }
   }
 }
 
-module.exports = {UserController}
+module.exports = { UserController };
