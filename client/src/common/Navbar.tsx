74174a4927e0808b1a892e6e5e15d38bab969f19
murphbra <murphbra@oregonstate.edu>
2022-04-19 22:25:22 -0700

    used useAuth() hook to create conditional login/logout button reflecting auth/login state

--- client/src/common/Navbar.tsx
+++ client/src/common/Navbar.tsx
@@ -1,22 +1,31 @@
-import { AppBar, Toolbar, Button, ButtonGroup, Link } from '@mui/material';
+import { AppBar, Toolbar, Button, ButtonGroup } from '@mui/material';
+import { useAuth } from './AuthContext';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
+  const auth = useAuth();
   return (
     <AppBar position="static" elevation={0} color="primary">
       <Toolbar>
         <ButtonGroup variant="contained" color="inherit">
           <Button>
-            <Link href="/" underline="none" variant="button">
+            <Link to="/" style={{ textDecoration: 'none' }}>
               Home
             </Link>
           </Button>
-          <Button>
-            <Link href="/login" underline="none" variant="button">
-              Login
-            </Link>
+          <Button onClick={() => (auth.isSignedIn ? auth.signOut() : auth.signIn())}>
+            {auth.isSignedIn ? (
+              <Link to="/login" style={{ textDecoration: 'none' }}>
+                Logout
+              </Link>
+            ) : (
+              <Link to="/login" style={{ textDecoration: 'none' }}>
+                Login
+              </Link>
+            )}
           </Button>
           <Button>
-            <Link href="/signup" underline="none" variant="button">
+            <Link to="/signup" style={{ textDecoration: 'none' }}>
               Signup
             </Link>
           </Button>
