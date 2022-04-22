192511033b2141a9829a0f7fccf6867edc24d55e
Adam <okashaa@oregonstate.edu>
2022-04-22 17:56:14 -0400

    Add attribute and association

--- models/job.js
+++ models/job.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Job extends Model {
     /**
@@ -11,17 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.User, { foreignKey: "userId" });
     }
   }
-  Job.init({
-    internship: DataTypes.BOOLEAN,
-    title: DataTypes.STRING,
-    company: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    link: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Job',
-  });
+  Job.init(
+    {
+      internship: DataTypes.BOOLEAN,
+      title: DataTypes.STRING,
+      company: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      link: DataTypes.TEXT,
+      userId: DataTypes.UUID,
+    },
+    {
+      sequelize,
+      modelName: "Job",
+    }
+  );
   return Job;
 };
