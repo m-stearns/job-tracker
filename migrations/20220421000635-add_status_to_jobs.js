577318ea294137ec59f35173ce3de42cbde1bd88
mikeybs1987 <michaelbstearns@gmail.com>
2022-04-23 23:21:09 -0700

    Changed 'title' and 'company' attributes in Jobs table to not allowNull. Migration was successful.

--- migrations/20220421000635-add_status_to_jobs.js
+++ migrations/20220421000635-add_status_to_jobs.js
@@ -16,11 +16,59 @@ module.exports = {
             transaction: t,
           }
         ),
+        queryInterface.changeColumn(
+          "Jobs", 
+          "title", 
+          {
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+          {
+            transaction: t,
+          }
+        ),
+        queryInterface.changeColumn(
+          "Jobs",
+          "company",
+          {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          {
+            transaction: t,
+          }
+        ),
       ]);
     });
   },
 
   async down (queryInterface, Sequelize) {
-    return queryInterface.removeColumn("Jobs", "status");
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn("Jobs", "status", {transaction: t}),
+        queryInterface.changeColumn(
+          "Jobs",
+          "title",
+          {
+            type: Sequelize.STRING,
+            allowNull: true 
+          }, 
+          {
+            transaction: t
+          }
+        ),
+        queryInterface.changeColumn(
+          "Jobs",
+          "company",
+          {
+            type: Sequelize.STRING,
+            allowNull: true 
+          }, 
+          {
+            transaction: t
+          }
+        ),
+      ]);
+    });
   }
 };
--- models/job.js
+++ models/job.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Job extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
@@ -30,7 +25,6 @@ module.exports = (sequelize, DataTypes) => {
     },
     link: {
       type: DataTypes.TEXT,
-      allowNull: false
     },
     status: {
       type: DataTypes.ENUM("Applied", "Interview Scheduled", "Decision Pending", "Accepted", "Rejected"),
