6312a24a58c13ae0335c5f900b4247593002ff1e
Kaewan Gardi <k1gardi@gmail.com>
2022-04-24 21:33:10 -0700

    home page component for no applications

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,33 @@
+// import { useState } from 'react';
+import {
+  Button,
+  Container,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+
 export const Home = () => {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+  return <NoContent />;
 };
+
+const NoContent = () => (
+  <Container maxWidth="sm">
+    <Paper elevation={10} style={{ padding: '32px', margin: '16px auto' }}>
+      <Stack spacing={6} justifyContent="center" alignItems="center">
+        <Typography sx={{ fontStyle: 'italic', pt: '8px' }}>Nothing to see here...</Typography>
+        <Button variant="contained" onClick={() => alert('TODO: go to create job page')}>
+          {/* <Link to="/create-job" style={{ textDecoration: 'none' }}> */}
+          Create new application
+          {/* </Link> */}
+        </Button>
+      </Stack>
+    </Paper>
+  </Container>
+);
