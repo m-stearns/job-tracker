7d4c4f02155ddbfe3a7d225b383cd1bfec4df7b3
Adam <okashaa@oregonstate.edu>
2022-05-07 14:26:35 -0400

    Add user -> skills association, refactor

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,35 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Job, Skill, JobSkills } = require("../models");
+const { JobController } = require("../controllers/job.controller");
 const requireAuth = require("../middlewares/requireAuth.middleware");
 
-router.get("/", requireAuth, async (req, res) => {
-  try {
-    const user = req.user;
-
-    const jobs = await Job.findAll({
-      where: {
-        userId: user.id,
-      },
-      include: [
-        {
-          model: JobSkills,
-          as: "skills",
-          include: [
-            {
-              model: Skill,
-              as: "skill",
-            }
-          ]
-        },
-      ],
-    });
-    
-    res.send(jobs);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.get("/", requireAuth, JobController.findAll);
 
 router.use("/jobs", router);
 
