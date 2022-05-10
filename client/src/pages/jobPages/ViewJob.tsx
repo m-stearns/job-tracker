8843004c550cb82a6b51cb511da47028bf279b16
Kaewan Gardi <k1gardi@gmail.com>
2022-05-09 19:45:32 -0700

    clean up TODOs

--- client/src/pages/jobPages/ViewJob.tsx
+++ client/src/pages/jobPages/ViewJob.tsx
@@ -35,7 +35,7 @@ export const ViewJob = () => {
 
   // TODO: Remove after API call
   const tempSuperFakeNotRealJob = {
-    title: 'George Maharis',
+    title: 'CTO',
     company: 'FakeBlock',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
