0080eb114eb7a3f94ea0d51ad48745e91ecff802
murphbra <murphbra@oregonstate.edu>
2022-05-15 17:32:20 -0700

    upDATE

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,11 +8,5 @@ router.get("/", requireAuth, ContactController.findAll);
 router.post("/create", requireAuth, ContactController.create);
 router.get("/current_contact", requireAuth, ContactController.currentContact)
 
-<<<<<<< HEAD
 
 module.exports = { contactsRouter: router };
-=======
-router.use("/contacts", router);
-
-module.exports = router;
->>>>>>> bd95daa9c1abdf2b59e95c5b955fbcf722fb9f89
